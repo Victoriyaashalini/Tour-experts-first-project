d1b7a75dcbb9e14235118bfc611e0d68540c98fe
Victoriyaashalini <165498537+Victoriyaashalini@users.noreply.github.com>
2024-04-08 16:08:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+
+import './App.css';
+import Navbar from './Components/layout/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/layout/Home';
+import About from './Components/layout/About';
+import Destination from './Components/layout/Destination';
+import Package from './Components/layout/Package';
+
+
+import Notfound from './Components/layout/Notfound';
+
+
+import Popular from './Components/layout/Popular';
+import Customize from './Components/layout/Customize';
+import Contact from './Components/layout/Contact';
+
+import CustompackageForm from './Components/forms/CustompackageForm';
+import Footer from './Components/layout/Footer';
+import Indiatour from './Components/cards/Indiatour';
+import Internationaltour from './Components/cards/Internationaltour';
+
+
+
+
+
+
+
+function App() {
+  return (
+   <Router>
+    <Navbar/>
+ 
+   
+ <Routes>
+        
+          <Route exact path='/' element={<Home/>}/>
+          <Route path="/about" element={<About/>} />
+          <Route path="/Destination" element={<Destination/>}/>
+          <Route path="/Package" element={<Package/>}/>
+          <Route path="/popular" element={<Popular/>}/>
+          <Route path="/customize" element={<Customize/>}/>
+         <Route path="/Contact" element={<Contact/>}/>
+        <Route path='/customizes' element={<CustompackageForm/>} />
+        <Route path="/indiatour" element={<Indiatour />} />
+         <Route path='/international' element={<Internationaltour/>}/>
+        
+          <Route component={Notfound} />
+          
+     
+   
+    
+          </Routes>
+    <Footer/>
+   </Router>
+   
+  );
+}
+
+export default App;
